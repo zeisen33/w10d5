faecc436f8e9aecbdc742c2d88543629f7bd1856
Zane <zeisen33@gmail.com>
2023-01-20 17:23:44 -0500

    Start Phase 2

--- art-museum/src/App.js
+++ art-museum/src/App.js
@@ -1,5 +1,6 @@
 import harvardArt from "./data/harvardArt.js"
 import GalleryNavigation from "./components/GalleryNavigation/index.js"
+import GalleryView from "./components/GalleryView/index.js"
 
 function App() {
   return (
---
+++ art-museum/src/components/GalleryView/index.js
@@ -0,0 +1,5 @@
+const Galleryview = () => {
+    <h1>Hello from GalleryView</h1>
+}
+
+export default Galleryview
